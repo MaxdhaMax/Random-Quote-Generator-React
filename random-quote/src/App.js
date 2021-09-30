7fb9bfe2f7568e541ba04f3261c3e677418da91b
MaxdhaMax <maxdhamaxi@gmail.com>
2021-09-30 13:35:18 +0700

    menambahkan semua kriteria free code camp

--- random-quote/src/App.js
+++ random-quote/src/App.js
@@ -1,11 +1,56 @@
-import './App.css';
+import React , {useState} from 'react'
+import { useEffect } from 'react'
+import { Button} from 'react-bootstrap'
+import { GoQuote } from 'react-icons/go'
+import './App.css'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Haiii</h1>
-    </div>
-  );
+const App = () => {
+    const [quote, setQuote] = useState("")
+    const [author, setAuthor] = useState("")
+
+    const quoteApi = async () => {
+      let arrayOfQuotes = []
+      try {
+        const data = await axios.get('https://api.quotable.io/random')
+        arrayOfQuotes = data.data
+        console.log(arrayOfQuotes)
+      } catch (error) {
+        console.log(error)
+      }
+
+      try {
+        setQuote(arrayOfQuotes.content)
+        setAuthor(arrayOfQuotes.author)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    useEffect(() => {
+      quoteApi()
+    }, [])
+
+    return (
+      <div className="App">
+        <section className="Main" id="quote-box">
+          <div className="main-content pb-5 mt-5 rounded container-sm">
+            <div className="row justify-content-center text-center">
+              <div className="col-6 mb-3 mt-5" id="text"><h3><GoQuote size="30px"/> {quote}</h3></div>
+            </div>
+            <div className="row">
+              <div className="col mb-2 author" id="author"><h5>- {author}</h5></div>
+            </div>
+            <div classname="row justify-content-evenly">
+            <div className="col-4"><a href="https://twitter.com/intent/tweet" target="_blank" id="tweet-quote" value={quote}>Twitter</a></div>
+              <div className="col-4 button-new" id="new-quote"><Button className="bg-success" onClick={quoteApi}>NEW QUOTES</Button> </div>
+            </div>
+          </div>
+          <div class="d-flex justify-content-center mt-3"><h5>By -Maxdha</h5></div>
+        </section>
+        
+      </div>
+    )
 }
 
 export default App;
--- random-quote/src/index.js
+++ random-quote/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free'
+
 import './index.css';
 import App from './App';
 
